e3c42af52b3be9fc68c75633c1ae48d18495a23f
KUOPING LEE <blu_blac@hotmail.com>
2016-09-26 02:43:13 +0800

    tictactoe draft 1

---
+++ script.js
@@ -0,0 +1,15 @@
+
+
+function refresh () {
+  window.location.reload();
+}
+
+var x = "X";
+var o = "O";
+var playerOne = x;
+
+// player click, X appears
+// switch player
+// loop for 9 times till end of game OR till one player wins
+
+// to win, ([1,2,3],[4,5,6,],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7])
