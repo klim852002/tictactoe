56f73ca230be8bbaaaa71391db43c3cc0277fca5
KUOPING LEE <blu_blac@hotmail.com>
2016-09-26 14:33:03 +0800

    added codes

---
+++ js/script.js
@@ -0,0 +1,52 @@
+document.addEventListener('DOMContentLoaded', init)
+
+function init () {
+  alert ('script loaded')
+}
+//   var x = "X";
+//   var o = "O";
+//   var playerOne = x;
+// var blocks = document.querySelectorAll('.blocks')
+// // console.log(blocks)
+//
+// for (var i = 0, i  < blocks.length; i++) {
+//   blocks[i].addEventListener('click', blockClick)
+// }
+// function blockClick () {
+//   of (currentPLayer === 1) {
+//     this.classList.add ('clickx')
+//   } else {
+//     this.classList.add ('clicko')
+//       }
+//     }
+//   } else {
+//
+//   }
+//   }
+//   // alert('box is clicked')
+//   // alert('box' + this.id + ' is clicked')
+//   // this.innerHTML = 'something'
+//   // this.style}
+// this.classList.add('clickx')
+// }
+//
+// function switchPlayer () {
+//   if (currentPLayer === 1) {
+//     currentPLayer = 2
+//   } else {
+//     currentPLayer = 1
+//   }
+// }
+//
+//
+function refresh () {
+  location.reload();
+}
+
+
+
+// player click, X appears
+// switch player
+// loop for 9 times till end of game OR till one player wins
+
+// var winnning ([1,2,3],[4,5,6,],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7])
